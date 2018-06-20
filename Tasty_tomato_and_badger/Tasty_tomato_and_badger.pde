864da68f0cc9f4236a1d4264ac2025bcfbdab2bf
AaronD07 <league@WTS14.attlocal.net>
2018-06-20 12:01:33 -0700

    TASTY TOMATO Y BADGER BADGER

---
+++ Tasty_tomato_and_badger/Tasty_tomato_and_badger.pde
@@ -0,0 +1,20 @@
+void setup() {
+    size(500, 500);
+}
+void draw() {
+    background(200, 200, 200);
+    fill(#9C9D9C);
+    noStroke();
+      fill(#FC0303);
+    ellipse(150, 200, 150, 150);
+    ellipse(212, 200, 150, 150);
+ fill(#05FF0F);  
+
+    rect(176, 103, 12, 32);
+        fill(#9C9D9C);
+        if(mousePressed) {
+    ellipse(70, 200, 50, 50);
+fill(#FC0303);
+}
+
+}
--- src/extra/badger.java
+++ src/extra/badger.java
@@ -2,16 +2,34 @@ package extra;
 
 public class badger {
 	public static void main(String[] args) {
-		String badger = "Badger";
-		String african = "African";
-		String mushroom = "Mushroom";
-		String snake = "Snake";
-		for (int i = 1; i < 11; i++) {
-			for (int x = 0; x < 2; x++) {
+		// Badger, badger, badger, badger, badger, badger, badger, badger, badger,
+		// badger
+		// Mushroom, mushroom, a
+		// Badger, badger, badger, badger, badger, badger, badger, badger, badger,
+		// badger
+		// Mushroom, mushroom, a
+		// Badger, badger, badger, badger, badger, badger, badger, badger, badger,
+		// badger
+		// Mush-mushroom, a
+		// Badger, badger, badger, badger, badger, badger, badger, badger, badger,
+		// badger
+		// African! Snake, a snake
+		// Snake! A snake, ooh it's a snake
+		// It's a badger, badger, badger, badger, badger, badger, badger, badger,
+		// badger, badger
+		// Mushroom, mushroom, a
+		for (int k = 0; k < 6; k++) {
 
+			for (int i = 0; i < 11; i++) {
+				System.out.println("Badger");
+			}
+			for (int x = 0; x < 2; x++) {
+				System.out.println("Mushroom");
+			}
+			for (int x = 0; x < 2; x++) {
+				System.out.println("Snake");
 			}
 
 		}
-
 	}
 }
